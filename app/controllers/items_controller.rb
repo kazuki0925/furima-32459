2e6872c7848be03051ced988a1d149d634e15f93
kazuki0925 <kazukinyo0925@gmail.com>
2020-12-06 16:29:15 +0900

    itemコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_sign_in, only: [:new, :create ]
+  before_action :authenticate_user!, only: [:new, :create ]
 
   def index
   end
@@ -20,11 +20,5 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :image, :info, :price, :category_id, :sales_status_id, :shopping_fee_status_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
-  end
-
-  def move_to_sign_in
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
   end
 end
