f8935ca397ab80c192bc028b7511a9dd05f66cc8
kazuki0925 <kazukinyo0925@gmail.com>
2020-12-09 17:46:30 +0900

    商品購入後の制限機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,7 +50,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if user_signed_in? && current_user.id != @item.user_id
+    if user_signed_in? && current_user.id != @item.user_id || @item.order
       redirect_to root_path
     end
   end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [:index, :create] 
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_order, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @order_form = OrderForm.new
@@ -35,6 +37,9 @@ class OrdersController < ApplicationController
     )
   end
 
-
-
+  def move_to_index
+    if Order.find_by(item_id: params[:item_id]) || current_user.id == @order.item.user_id
+      redirect_to root_path
+    end
+  end
 end
